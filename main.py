4a4397b2a87c84cc9f9ab28755fe7a5afb74360c
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2022-10-16 22:40:33 +0900

    kirby pos adjusted

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from pico2d import*
 import game_framework
 import map_state
 import logo_state
+import move_kirby
 
 states = [logo_state, map_state]
 def state_print(a):
+    clear_canvas()
     a.enter()
     a.draw()
     a.update()
     a.handle_events()
-
 
 open_canvas(800, 400)
 
