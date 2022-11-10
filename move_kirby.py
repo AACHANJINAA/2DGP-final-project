3c47f6f073663c03e1def516878fe0effd54a9f0
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2022-11-11 01:08:46 +0900

    change.kirby_jump.png

--- move_kirby.py
+++ move_kirby.py
@@ -35,10 +35,10 @@ class IDLE:
     def draw(self):
         if self.face_dir_x == -1:
             self.Idle.clip_composite_draw(int(self.frame) * 23, 0, 22, 18,
-                                           0.0, '', self.x, self.y, self.kx, self.ky)
+                                           0.0, '', self.x, self.y, self.kx - 1, self.ky - 1)
         else:
             self.Idle.clip_composite_draw(int(self.frame) * 23, 0, 22, 18,
-                                           0.0, 'h', self.x, self.y, self.kx, self.ky)
+                                           0.0, 'h', self.x, self.y, self.kx - 1, self.ky - 1)
 class RUN:
     def enter(self, event):
         print('ENTER RUN')
@@ -69,32 +69,32 @@ class RUN:
 class JUMP:
     def enter(self, event):
         print('ENTER RUN')
-        if event == RD:
-            self.dir_x += 1
-        elif event == LD:
-            self.dir_x -= 1
-        elif event == RU:
-            self.dir_x -= 1
-        elif event == LU:
-            self.dir_x += 1
+        if event == JD:
+            self.dir_y = 1
+            self.timer = 70
+            self.j_timer = self.timer // 2
 
     def exit(self, event):
         print('EXIT RUN')
-        self.face_dir_x = self.dir_x
+
     def do(self):
         self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
-        self.x += self.dir_x * RUN_SPEED_PPS * game_framework.frame_time
-        #print(RUN_SPEED_MPM)
-        print(game_framework.frame_time)
-        self.x = clamp(0, self.x, 800)
+        jump_dis = self.dir_y * RUN_SPEED_PPS * game_framework.frame_time
+        self.y += jump_dis
+        self.timer -= 1
+        if self.timer == self.j_timer:
+            self.dir_y *= -1
+        elif self.timer == 0:
+            self.dir_y *= -1
+            self.add_event(TIMER)
 
     def draw(self):
-        if self.dir_x == 1:
-            self.Run.clip_draw(int(self.frame) * self.kx, self.ky - self.dir_y * self.ky,
-                               self.kx, self.ky, self.x, self.y)
+        if self.face_dir_x == -1:
+            self.Jump.clip_composite_draw(int(self.frame) * 25, 0, 25, 22,
+                                           0.0, '', self.x, self.y, self.kx, self.ky)
         else:
-            self.Run.clip_draw(int(self.frame) * self.kx, self.ky,
-                               self.kx, self.ky, self.x, self.y)
+            self.Jump.clip_composite_draw(int(self.frame) * 25, 0, 25, 22,
+                                           0.0, 'h', self.x, self.y, self.kx, self.ky)
 class SLEEP:
     def enter(self, event):
         print('ENTER SLEEP')
@@ -113,9 +113,10 @@ class SLEEP:
                                            0.0, 'h', self.x, self.y, self.kx, self.ky)
 
 next_state = {
-    IDLE:  {RU: RUN,  LU: RUN,  RD: RUN,  LD: RUN, TIMER: SLEEP},
-    RUN:   {RU: IDLE, LU: IDLE, RD: IDLE, LD: IDLE},
-    SLEEP: {RU: RUN, LU: RUN, RD: RUN, LD: RUN}
+    IDLE:  {RU: RUN,  LU: RUN,  RD: RUN,  LD: RUN,  JD: JUMP, TIMER: SLEEP},
+    RUN:   {RU: IDLE, LU: IDLE, RD: IDLE, LD: IDLE, JD: JUMP, TIMER: IDLE},
+    SLEEP: {RU: RUN,  LU: RUN,  RD: RUN,  LD: RUN,  JD: IDLE, TIMER: IDLE},
+    JUMP:  {RU: JUMP, LU: JUMP, RD: JUMP, LD: JUMP, JD: JUMP, TIMER: IDLE}
 }
 PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
 RUN_SPEED_KMPH = 20.0 # Km / Hour
@@ -131,10 +132,11 @@ class Kirby:
         self.x, self.y = 30, 100
         self.kx, self.ky = 70, 55
         self.face_dir_x, self.dir_x = 1, 0
+        self.dis_y = self.y
         self.dir_y = 1
-        self.jump_y = 1
         self.frame = 7
         self.timer = 0
+        self.j_timer = 0
 
         self.event_que = []
         self.cur_state = IDLE
