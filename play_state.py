76c764250a333c11bb089bdbede16a4a522cedc0
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2022-11-11 18:02:17 +0900

    add.kirby(spark)

--- play_state.py
+++ play_state.py
@@ -4,10 +4,12 @@ import game_world
 
 from move_kirby import Kirby
 from move_sword import Sword_Kirby
+from move_spark import Spark_Kirby
 
 stage = None
 kirby = None
 sword_kirby = None
+spark_kirby = None
 
 def handle_events():
     events = get_events()
@@ -19,12 +21,14 @@ def handle_events():
         else:
             kirby.handle_event(event)
             sword_kirby.handle_event(event)
+            spark_kirby.handle_event(event)
 
 def enter():
-    global kirby, sword_kirby
+    global kirby, sword_kirby, spark_kirby
     kirby = Kirby()
     sword_kirby = Sword_Kirby()
-    game_world.add_object(sword_kirby, 0)
+    spark_kirby = Spark_Kirby()
+    game_world.add_object(spark_kirby, 0)
 
 def exit():
     game_world.clear()
