c37cb79231a0a8bc2ffa69c28980c3e281cb1fcd
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2022-11-12 18:17:07 +0900

    add.last_boss
    이미지 + 움직임 구현

--- boss.py
+++ boss.py
@@ -46,6 +46,7 @@ class IDLE2:
 class IDLE3:
     @staticmethod
     def enter(self, event):
+        self.x, self.y = 700, 130
         self.frame = 0
         self.dir = -1
 
@@ -66,6 +67,32 @@ class IDLE3:
                                           0.0, 'h', self.x, self.y, self.kx, self.ky)
         else:
             self.Idle.clip_composite_draw(int(self.frame) * 48, 0, 48, 56,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+
+class IDLE4:
+    @staticmethod
+    def enter(self, event):
+        self.x, self.y = 700, 130
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
+        if self.x < 100 or self.x > 700:
+            self.dir *= -1
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Idle.clip_composite_draw(int(self.frame) * 48, 0, 48, 49,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Idle.clip_composite_draw(int(self.frame) * 48, 0, 48, 49,
                                           0.0, '', self.x, self.y, self.kx, self.ky)
 
 PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
@@ -151,6 +178,37 @@ class BOSS3:
         self.cur_state.enter(self, None)
 
         self.Idle = load_image('boss/sword_boss.png')
+
+    def update(self):
+        self.cur_state.do(self)
+
+        if self.event_que:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+
+            self.cur_state.enter(self, event)
+
+    def draw(self):
+        self.cur_state.draw(self)
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+    def handle_event(self, event):
+        pass
+
+class BOSS4:
+    def __init__(self):
+        self.x, self.y = 700, 130
+        self.kx, self.ky = 96, 112
+        self.frame = 0
+        self.dir = -1
+
+        self.event_que = []
+        self.cur_state = IDLE4
+        self.cur_state.enter(self, None)
+
+        self.Idle = load_image('boss/last_boss.png')
 
     def update(self):
         self.cur_state.do(self)
--- play_state.py
+++ play_state.py
@@ -8,7 +8,7 @@ from move_spark import Spark_Kirby
 from move_bomber import Bomber_Kirby
 from Background import SWORD_BACK, SPARK_BACK, BOMBER_BACK
 from Background import SWORD_BOSS, SPARK_BOSS, BOMBER_BOSS, LAST_BOSS
-from boss import BOSS1, BOSS2, BOSS3
+from boss import BOSS1, BOSS2, BOSS3, BOSS4
 
 kirby, sword_kirby, spark_kirby, bomber_kirby = None, None, None, None
 stage1, stage2, stage3 = None, None, None
@@ -50,11 +50,13 @@ def enter():
     stage6 = BOMBER_BOSS(0)
     stage7 = LAST_BOSS(0)
 
+    # 봄버 스파크 칼 디디디마왕순
     boss1 = BOSS1()
     boss2 = BOSS2()
     boss3 = BOSS3()
+    boss4 = BOSS4()
 
-    name = 'sword'
+    name = 'sword_boss'
 
     if name == 'sword':
         b = sword_kirby
@@ -78,6 +80,7 @@ def enter():
         b = bomber_kirby
         a = stage6
     elif name == 'last':
+        c = boss4
         b = kirby
         a = stage7
     
