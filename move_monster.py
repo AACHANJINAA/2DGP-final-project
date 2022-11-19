f8280c615d6bf0ec3341aca12292de49ff9d4504
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2022-11-19 23:20:07 +0900

    add.four_HIT_state
    basic, sword, spark, bomber 기본 몬스터들의 피해 class를 구현

--- move_monster.py
+++ move_monster.py
@@ -1,116 +1,8 @@
 from pico2d import*
 import game_framework
 
-class IDLE1:
-    @staticmethod
-    def enter(self, event):
-        self.frame = 0
-        self.dir = -1
-
-    @staticmethod
-    def exit(self, event):
-        pass
-    @staticmethod
-    def do(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
-        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time / 2
-        if self.x < 100 or self.x > 700:
-            self.dir *= -1
-
-    @staticmethod
-    def draw(self):
-        if self.dir == -1:
-            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 19,
-                                          0.0, '', self.x, self.y, self.kx, self.ky)
-        else:
-            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 19,
-                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
-
-class IDLE2:
-    @staticmethod
-    def enter(self, event):
-        self.frame = 0
-        self.dir = -1
-
-    @staticmethod
-    def exit(self, event):
-        pass
-    @staticmethod
-    def do(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
-        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time / 2
-        if self.x < 100 or self.x > 700:
-            self.dir *= -1
-
-    @staticmethod
-    def draw(self):
-        if self.dir == -1:
-            self.Idle.clip_composite_draw(int(self.frame) * 27, 0, 27, 23,
-                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
-        else:
-            self.Idle.clip_composite_draw(int(self.frame) * 27, 0, 27, 23,
-                                          0.0, '', self.x, self.y, self.kx, self.ky)
-
-class IDLE3:
-    @staticmethod
-    def enter(self, event):
-        self.frame = 0
-        self.dir = -1
-
-    @staticmethod
-    def exit(self, event):
-        pass
-    @staticmethod
-    def do(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 2
-        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time / 2
-        if self.x < 100 or self.x > 700:
-            self.dir *= -1
-
-    @staticmethod
-    def draw(self):
-        if self.dir == -1:
-            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 19,
-                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
-        else:
-            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 19,
-                                          0.0, '', self.x, self.y, self.kx, self.ky)
-
-class IDLE4:
-    @staticmethod
-    def enter(self, event):
-        self.frame = 0
-        self.dir = -1
-
-    @staticmethod
-    def exit(self, event):
-        pass
-    @staticmethod
-    def do(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
-        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time / 2
-        if self.x < 100 or self.x > 700:
-            self.dir *= -1
-
-    @staticmethod
-    def draw(self):
-        if self.dir == -1:
-            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 26,
-                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
-        else:
-            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 26,
-                                          0.0, '', self.x, self.y, self.kx, self.ky)
-
-PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
-RUN_SPEED_KMPH = 20.0 # Km / Hour
-RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
-RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
-RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
-
-TIME_PER_ACTION = 2.0
-ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 8
-
+# 이 리스트로 맞는 방향 저장 -> hit클래스에 넣어주기
+hit_dir = [None, None, None, None]
 class BASIC_MONSTER:
 
     def __init__(self):
@@ -120,7 +12,7 @@ class BASIC_MONSTER:
         self.dir = -1
 
         self.event_que = []
-        self.cur_state = IDLE1
+        self.cur_state = IDLE
         self.cur_state.enter(self, None)
 
         self.Idle = load_image('basic_monster/basic/basic_monster.png')
@@ -152,7 +44,7 @@ class SWORD_MONSTER:
         self.dir = -1
 
         self.event_que = []
-        self.cur_state = IDLE2
+        self.cur_state = IDLE
         self.cur_state.enter(self, None)
 
         self.Idle = load_image('basic_monster/sword/sword_monster.png')
@@ -184,7 +76,7 @@ class SPARK_MONSTER:
         self.dir = -1
 
         self.event_que = []
-        self.cur_state = IDLE3
+        self.cur_state = IDLE
         self.cur_state.enter(self, None)
 
         self.Idle = load_image('basic_monster/spark/spark_monster.png')
@@ -216,7 +108,7 @@ class BOMBER_MONSTER:
         self.dir = -1
 
         self.event_que = []
-        self.cur_state = IDLE4
+        self.cur_state = IDLE
         self.cur_state.enter(self, None)
 
         self.Idle = load_image('basic_monster/bomber/bomber_monster.png')
@@ -239,6 +131,204 @@ class BOMBER_MONSTER:
 
     def handle_event(self, event):
         pass
+
+class IDLE(BASIC_MONSTER):
+    @staticmethod
+    def enter(self, event):
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time / 2
+        if self.x < 100 or self.x > 700:
+            self.dir *= -1
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 19,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 19,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+
+class IDLE(SWORD_MONSTER):
+    @staticmethod
+    def enter(self, event):
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time / 2
+        if self.x < 100 or self.x > 700:
+            self.dir *= -1
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Idle.clip_composite_draw(int(self.frame) * 27, 0, 27, 23,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Idle.clip_composite_draw(int(self.frame) * 27, 0, 27, 23,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+
+class IDLE(SPARK_MONSTER):
+    @staticmethod
+    def enter(self, event):
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 2
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time / 2
+        if self.x < 100 or self.x > 700:
+            self.dir *= -1
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 19,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 19,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+
+class IDLE(BOMBER_MONSTER):
+    @staticmethod
+    def enter(self, event):
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time / 2
+        if self.x < 100 or self.x > 700:
+            self.dir *= -1
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 26,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Idle.clip_composite_draw(int(self.frame) * 22, 0, 22, 26,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+
+class HIT(BASIC_MONSTER):
+    @staticmethod
+    def enter(self, event):
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.x += self.dir * RUN_SPEED_PPS
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Hit.clip_composite_draw(0, 0, 21, 18,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Hit.clip_composite_draw(0, 0, 21, 18,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+class HIT(SWORD_MONSTER):
+    @staticmethod
+    def enter(self, event):
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.x += self.dir * RUN_SPEED_PPS
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Hit.clip_composite_draw(0, 0, 18, 25,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Hit.clip_composite_draw(0, 0, 18, 25,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+class HIT(SPARK_MONSTER):
+    @staticmethod
+    def enter(self, event):
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.x += self.dir * RUN_SPEED_PPS
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Hit.clip_composite_draw(0, 0, 18, 17,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Hit.clip_composite_draw(0, 0, 18, 17,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+class HIT(BOMBER_MONSTER):
+    @staticmethod
+    def enter(self, event):
+        self.frame = 0
+        self.dir = -1
+
+    @staticmethod
+    def exit(self, event):
+        pass
+    @staticmethod
+    def do(self):
+        self.x += self.dir * RUN_SPEED_PPS
+
+    @staticmethod
+    def draw(self):
+        if self.dir == -1:
+            self.Hit.clip_composite_draw(0, 0, 19, 26,
+                                          0.0, '', self.x, self.y, self.kx, self.ky)
+        else:
+            self.Hit.clip_composite_draw(0, 0, 19, 26,
+                                          0.0, 'h', self.x, self.y, self.kx, self.ky)
+
+
+PIXEL_PER_METER = (10.0 / 0.3) # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0 # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 2.0
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
 
 
 
